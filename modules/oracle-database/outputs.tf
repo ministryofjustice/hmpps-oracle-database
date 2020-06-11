b45ed809a8b127fceac3017257fe18c42560bbeb
Stephen James <smjmoj@gmail.com>
2020-06-11 17:51:04 +0100

    Add ha tf state output (#10)
    * test output
    
    * instance, r53 ids
    
    * instance, r53 ids
    
    * adding route53 details to output

--- modules/oracle-database/outputs.tf
+++ modules/oracle-database/outputs.tf
@@ -24,3 +24,25 @@ output "db_size_parameters" {
     iops_ratio     = "${local.iops/local.size} (50 max)"
   }
 }
+
+output "db_tf_import" {
+  value = {
+    instance_id     = "${aws_instance.oracle_db.id}"
+    r53_id_internal = "${aws_route53_record.oracle_db_instance_internal.zone_id}-${aws_route53_record.oracle_db_instance_internal.name}_${aws_route53_record.oracle_db_instance_internal.type}"
+    r53_id_public   = "${aws_route53_record.oracle_db_instance_public.zone_id}-${aws_route53_record.oracle_db_instance_public.name}_${aws_route53_record.oracle_db_instance_public.type}"
+  }
+}
+
+output "r53_id_public" {
+  value = {
+    name = "${aws_route53_record.oracle_db_instance_public.name}"
+    fqdn = "${aws_route53_record.oracle_db_instance_public.fqdn}"
+  }
+}
+
+output "r53_id_internal" {
+  value = {
+    name = "${aws_route53_record.oracle_db_instance_internal.name}"
+    fqdn = "${aws_route53_record.oracle_db_instance_internal.fqdn}"
+  }
+}
