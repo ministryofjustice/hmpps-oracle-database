ccfeabdcb7c6b05f899d7922854499c7cd1e3216
adrianweetman <adrian.weetman@digital.justice.gov.uk>
2022-05-31 11:24:23 +0100

    Add missing local module variable

--- modules/oracle-database/modules/ebs-volume/variables.tf
+++ modules/oracle-database/modules/ebs-volume/variables.tf
@@ -8,9 +8,22 @@ variable "size" {
   default     = 25
 }
 
+variable "type" {
+  description = "The type of EBS volume. Can be standard, gp2, gp3, io1, io2, sc1 or st1 (Default: gp2)"
+  default     = "io1"
+  type        = string
+}
+
+
 variable "iops" {
-  description = "volume iops"
+  description = "The amount of IOPS to provision for the disk. Only valid for type of io1, io2 or gp3."
   default     = 1000
+}
+
+variable "throughput" {
+  description = "The throughput that the volume supports, in MiB/s. Only valid for type of gp3."
+  default     = 125
+  type        = number
 }
 
 variable "instance_id" {
